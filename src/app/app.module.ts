47feb5815227c9dbe8daa90fa120a9a43362bb7f
Purohit <ashish.purohit@accenture.com>
2017-09-20 13:57:16 +0530

    Validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -12,7 +14,7 @@ import { RegisterComponent } from './register/register.component';
     RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, NgbModule.forRoot(),FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,13 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, AbstractControl, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-register',
+  selector: 'register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  formGroup:FormGroup;
+  fname:AbstractControl;
+  lname:AbstractControl;
+  mobileNumber:AbstractControl;
+  email:AbstractControl;
+  password:AbstractControl;
+  cpassword:AbstractControl;
+  constructor(private formBuilder:FormBuilder) 
+  { 
+    this.formGroup= formBuilder.group
+    ({
+      "fname" : ['', Validators.compose([Validators.required,Validators.maxLength(16), Validators.minLength(3)])],
+      "lname" : ['', Validators.compose([Validators.required,Validators.maxLength(16),Validators.minLength(3)])],
+      "mobileNumber" : ['', Validators.compose([Validators.required,Validators.minLength(10),Validators.maxLength(14)])],
+      "email" : ['',Validators.compose([Validators.required,Validators.email,Validators.minLength(8),Validators.maxLength(24)])],
+      "password": ['',Validators.compose([Validators.required,Validators.pattern(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/)])],
+      "cpassword" : ['',Validators.compose([Validators.required,Validators.pattern(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/)])],
+    })
+
+    this.fname=this.formGroup.controls['fname'];
+    this.lname=this.formGroup.controls['lname'];
+    this.mobileNumber=this.formGroup.controls['mobileNumber'];
+    this.email=this.formGroup.controls['email'];
+    this.password=this.formGroup.controls['password'];
+    this.cpassword=this.formGroup.controls['cpassword'];
+  }
+  onSubmit(value):void
+  {
+    console.log(value);
+    console.log(this.fname.valid);
+  }  
 
   ngOnInit() {
   }
